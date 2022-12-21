51b0a0f9b80d18b2e7e1473c8c7345def9b06745
chadm9 <chadm_@yahoo.com>
2022-12-21 09:42:07 -0500

    init commit

---
+++ src/main/java/com/example/springbootexample/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.example.springbootexample.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+  @GetMapping("/hello")
+  ResponseEntity<String> hello() {
+    return new ResponseEntity<>("hello", HttpStatus.OK);
+  }
+
+}
